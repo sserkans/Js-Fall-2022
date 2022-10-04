72385095e46a64f7bffd49e23d3823225f77a6ee
srknsnmz <serkansonmez@gmail.com>
2022-10-04 13:16:51 -0400

    updated

--- Modules/Class3/operators.js
+++ Modules/Class3/operators.js
@@ -1,4 +1,11 @@
-let myTeamName = 'BJK';
-let myTeamSize = '10';
-++myTeamSize;
-console.log('I am part of' +  myTeamName + ', we have total' +  myTeamSize  + 'member');
+/**
+ * Operators in JS:
+ * 
+ * 1. Assignment Operator (=)
+ * 2. Arithmetic Operators (+, -, *, /, %, ++, --)
+ * 3. Comparison Operators (===, >=, <=, >, <, !==)
+ * 4. Logical Operators (&&, ||, !)
+ * 5. Ternary Operator (?:)
+ * 6. Advanced Assignment Operators (+=, -=, *=, /=, %=)
+ * 
+ */
